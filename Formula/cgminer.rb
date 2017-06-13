dc7777dd0d9b40ef5a02f9dff18e66e93040be89
Dainel Vera <arubinofaux@gmail.com>
2017-06-13 11:54:23 -0400

    cgminer

---
+++ Formula/cgminer.rb
@@ -0,0 +1,40 @@
+require 'formula'
+
+class Cgminer < Formula
+    homepage 'https://github.com/ckolivas/cgminer'
+    head 'https://github.com/ckolivas/cgminer.git', :branch => 'master'
+    url 'https://github.com/ckolivas/cgminer.git', :tag => 'v4.10.0'
+    version '4.10.0'
+
+    depends_on 'autoconf' => :build
+    depends_on 'automake' => :build
+    depends_on 'libtool' => :build
+    depends_on 'pkg-config' => :build
+    depends_on 'coreutils' => :build
+    depends_on 'curl'
+
+    def install
+        inreplace "autogen.sh", "libtoolize", "glibtoolize" 
+        inreplace "autogen.sh", "readlink", "greadlink"
+        system "autoreconf -fvi" 
+        system "./autogen.sh", "--disable-debug", "--disable-dependency-tracking",
+        "--prefix=#{prefix}",
+        "PKG_CONFIG_PATH=#{HOMEBREW_PREFIX}/opt/curl/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/jansson/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/libusb/lib/pkgconfig",
+        "--enable-scrypt",
+        "--enable-opencl",
+        "--enable-bflsc",
+        "--enable-bitforce",
+        "--enable-icarus",
+        "--enable-modminer",
+        "--enable-ztex",
+        "--enable-avalon",
+        "--enable-bitfury",
+        "--enable-hashfast",
+        "--enable-klondike"
+        system "make", "install"
+    end
+
+    test do
+        system "cgminer"
+    end
+end
